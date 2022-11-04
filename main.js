db7994ff8448f2a46a09f7416788bc4e22cb7c22
Isa Kaillany <isakaillany123@gmail.com>
2022-11-04 19:41:24 -0300

    add title

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function createGame(player1, hour, player2) {
     return `
         <li>
-            <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+            <img src="./assets/icon-${player1}.svg" title=${player1} alt="Bandeira do ${player1}" />
             <strong>${hour}</strong>
-            <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />            
+            <img src="./assets/icon-${player2}.svg" title=${player2} alt="Bandeira da ${player2}" />            
         </li>                 
     `
 }
@@ -28,11 +28,11 @@ document.querySelector('#cards').innerHTML =
         createGame("brazil", "16:00", "serbia")) +
 
         createCard("28/11", "segunda", 
-        createGame("korea", "10:00", "ghana") + 
+        createGame("south-korea", "10:00", "ghana") + 
         createGame("brazil", "13:00", "switzerland") + 
         createGame("portugal", "16:00", "uruguay")) +
         
         createCard("02/12", "sexta", 
-        createGame("korea", "07:00", "portugal") + 
+        createGame("south-korea", "07:00", "portugal") + 
         createGame("ghana", "12:00", "uruguay") + 
         createGame("brazil", "16:00", "cameroon"))
