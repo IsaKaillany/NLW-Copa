53bf23615dd0bf46dded6e940ae2c9cb8cac8aaa
Isa Kaillany <isakaillany123@gmail.com>
2022-11-03 21:04:55 -0300

    Animações e transições Aula 04

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 function createGame(player1, hour, player2) {
     return `
-    <li>
-        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
-        <strong>${hour}</strong>
-        <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">            
-    </li>                 
+        <li>
+            <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+            <strong>${hour}</strong>
+            <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />            
+        </li>                 
     `
 }
 
+let delay = -0.3;
 function createCard(date, day, games) {
+    delay += 0.3;
     return `
-    <div class="card">
+        <div class="card" style="animation-delay: ${delay}s">
             <h2>${date} <span>${day}</span></h2>                
             <ul> 
                 ${games}                     
